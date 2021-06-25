b241fcb5a6b70dfd5b1e09e0ac3102ec899dcb1d
Andrzej Padlo <andrzejpadlo@gmail.com>
2021-06-25 09:40:55 +0200

    Password generator first commit!

---
+++ generator/views.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from django.http import HttpResponse
+from django.shortcuts import render
+import random
+
+# Create your views here.
+def home(request):
+    return render(request, 'generator/home.html')
+
+def password(request):
+
+    characters = list('abcdefghijklmnopqrstuvwxyz')
+
+    if request.GET.get('uppercase'):
+        characters.extend(list('ABCDEFGHIJKLMNOPQRSTUVWXYZ'))
+    if request.GET.get('special'):
+        characters.extend(list('!@#$%^&*+-?'))
+    if request.GET.get('numbers'):
+        characters.extend(list('0123456789'))
+
+    length = int(request.GET.get('length', 12)) # default=12 characters long
+
+    pwd = ''
+
+    for x in range(length):
+        pwd += random.choice(characters)
+
+    return render(request, 'generator/password.html', {'password': pwd})
+
+def about(request):
+    return render(request, 'generator/about.html')
+
+
+
+
